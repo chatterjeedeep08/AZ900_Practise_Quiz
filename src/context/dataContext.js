587618b2336951456fd032321a2f524e5d0a1400
Deepargh Chatterjee <54524272+chatterjeedeep08@users.noreply.github.com>
2024-01-05 03:32:20 +0530

    Update dataContext.js

--- src/context/dataContext.js
+++ src/context/dataContext.js
@@ -2,39 +2,88 @@ import { createContext, useState, useEffect } from "react";
 
 const DataContext = createContext({});
 
-export const DataProvider = ({children}) => {
-      // All Quizs, Current Question, Index of Current Question, Answer, Selected Answer, Total Marks
+export const DataProvider = ({ children }) => {
+  // All Quizs, Current Question, Index of Current Question, Answer, Selected Answer, Total Marks
   const [quizs, setQuizs] = useState([]);
-  const [question, setQuesion] = useState({});
+  const [selectedQuestions, setSelectedQuestions] = useState([]);
+  const [question, setQuestion] = useState({});
   const [questionIndex, setQuestionIndex] = useState(0);
   const [correctAnswer, setCorrectAnswer] = useState('');
   const [selectedAnswer, setSelectedAnswer] = useState('');
   const [marks, setMarks] = useState(0);
+  const [flag, setFlag] = useState(true); // [true, false
+  const [Timer, setTimer] = useState(5100);
+  const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
 
   // Display Controlling States
   const [showStart, setShowStart] = useState(true);
   const [showQuiz, setShowQuiz] = useState(false);
   const [showResult, setShowResult] = useState(false);
-
+  
+  useEffect(() => {
+    setFlag(true);
+  },[]);
   // Load JSON Data
   useEffect(() => {
+    //console.log("react: useEffect: load JSON data");
     fetch('quiz.json')
       .then(res => res.json())
       .then(data => setQuizs(data))
-  }, []);
+    }, []);
 
+  useEffect(() => {
+    //console.log("react: useEffect: question slice here");
+    //console.log(quizs.length);
+    //console.log(flag);
+    if(quizs.length > 0 && flag){
+      const shuffledQuestions = quizs.sort(() => 0.5 - Math.random());
+      //console.log(shuffledQuestions.slice(0, 10));
+      setSelectedQuestions(shuffledQuestions.slice(0, 60));
+      setFlag(false);
+    }
+  },[flag,quizs,selectedQuestions]);
+
+
+  // const getRandomQuestions = () => {
+  //   if(flag){
+  //     const shuffledQuestions = quizs.sort(() => 0.5 - Math.random());
+  //     console.log(shuffledQuestions.slice(0, 10));
+  //     setSelectedQuestions(shuffledQuestions.slice(0, 10));
+  //   }
+  //   setFlag(false);
+  // };
+
+  // useEffect(() => {
+  //   setQuestion(selectedQuestions[questionIndex]);
+  //   console.log(question);
+  // }, [selectedQuestions, question, questionIndex]);
+    
   // Set a Single Question
   useEffect(() => {
-    if (quizs.length > questionIndex) {
-      setQuesion(quizs[questionIndex]);
-    }
-  }, [quizs, questionIndex])
+    //console.log("react: useEffect: set a single question");
+    setQuizs(selectedQuestions);
+    setQuestion(selectedQuestions[questionIndex]);
+  }, [selectedQuestions, questionIndex]);
+  
+  // Function to get 60 random questions
 
   // Start Quiz
   const startQuiz = () => {
     setShowStart(false);
     setShowQuiz(true);
+    startTimer();
   }
+
+  const startTimer = async () => {
+    for (let i = 5100; i >= 0; i--) {
+      setTimer(i);
+      console.log(i);
+      await delay(1000); // Wait for 1 second
+    }
+    showTheResult();
+  };
+  
 
   // Check Answer
   const checkAnswer = (event, selected) => {
@@ -44,14 +93,14 @@ export const DataProvider = ({children}) => {
 
       if (selected === question.answer) {
         event.target.classList.add('bg-success');
-        setMarks(marks + 5);
+        setMarks(marks + 0.8);
       } else {
         event.target.classList.add('bg-danger');
       }
     }
   }
 
-  // Next Quesion
+  // Next Question
   const nextQuestion = () => {
     setCorrectAnswer('');
     setSelectedAnswer('');
@@ -60,7 +109,9 @@ export const DataProvider = ({children}) => {
     const rightBtn = document.querySelector('button.bg-success');
     rightBtn?.classList.remove('bg-success');
     setQuestionIndex(questionIndex + 1);
+    setQuestion(quizs[questionIndex + 1]);
   }
+
 
   // Show Result
   const showTheResult = () => {
@@ -71,28 +122,18 @@ export const DataProvider = ({children}) => {
 
   // Start Over
   const startOver = () => {
-    setShowStart(false);
-    setShowResult(false);
-    setShowQuiz(true);
-    setCorrectAnswer('');
-    setSelectedAnswer('');
-    setQuestionIndex(0);
-    setMarks(0);
-    const wrongBtn = document.querySelector('button.bg-danger');
-    wrongBtn?.classList.remove('bg-danger');
-    const rightBtn = document.querySelector('button.bg-success');
-    rightBtn?.classList.remove('bg-success');
+    window.location.reload();
   }
-    return (
-        <DataContext.Provider value={{
-            startQuiz,showStart,showQuiz,question,quizs,checkAnswer,correctAnswer,
-            selectedAnswer,questionIndex,nextQuestion,showTheResult,showResult,marks,
-            startOver
-        }} >
-            {children}
-        </DataContext.Provider>
-    );
+
+  return (
+    <DataContext.Provider value={{
+      startQuiz, showStart, showQuiz, question, quizs, checkAnswer, correctAnswer,
+      selectedAnswer, questionIndex, nextQuestion, showTheResult, showResult, marks, Timer,
+      startOver
+    }} >
+      {children}
+    </DataContext.Provider>
+  );
 }
 
 export default DataContext;
-
